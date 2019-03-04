ed0a26a8c9196e6506ce2cebd5ccd99e6e476259
Kirill Klenov <horneds@gmail.com>
2019-03-04 15:23:03 +0300

    build(version): 0.1.11 -> 0.2.0

--- aiorest_client.py
+++ aiorest_client.py
@@ -11,7 +11,7 @@ from aiohttp.web import HTTPError
 
 __author__ = "Kirill Klenov"
 __author_email__ = "horneds@gmail.com"
-__version__ = "0.1.11"
+__version__ = "0.2.0"
 __license__ = "MIT license"
 
 
